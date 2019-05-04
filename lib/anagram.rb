4b11ef58ed0a37fabddae53efbaab4e6c8319565
Oscar Shoudt <shoudto@gmail.com>
2019-05-04 20:45:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,24 +1,21 @@
 require 'pry'
 # Your code goes here!
-class Anagram 
-  
+class Anagram
   attr_accessor :word 
   
-  def initialized(word)
-    @word = word  
+  def initialize(word)
+    @word = word
   end
   
-  def match(array)
-    # %w(array).each do |word| => I don't know why this didn't change into an array?
-      word_array = []
-      array.each do |word|
-      if word.split("").sort == word.split("").sort
-        word_array << word 
-      else 
-        word_array 
-      end 
-      word_array 
+  def match (array)
+    new_array = []
+    array.each do |word|
+      if word.split("").sort == @word.split("").sort
+        new_array << word
+      else
+        new_array
+      end
     end 
-  end 
-
+  new_array
+  end
 end 
