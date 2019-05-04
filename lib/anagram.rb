fead49451a926f3a7ab06fd54043436842593b43
Oscar Shoudt <shoudto@gmail.com>
2019-05-04 21:05:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match (array)
+    
+    anagram_array = []
+    
+    #w%(array).each do |word| => Why this didn't turn into an array 
+    #binding.pry 
+    
+    array.each do |word|
+      if word.split("").sort == @word.split("").sort
+        anagram_array << word
+      else
+        anagram_array
+      end
+    end 
+  anagram_array 
+  end
+  
+end 
